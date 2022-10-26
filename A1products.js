c9d78ab1f9f3b2469782c61885f6ffeda37d720c
zmatsudo@hawaii.edu <112070149+zmatsudo@users.noreply.github.com>
2022-10-26 00:33:28 -1000

    Initial commit

---
+++ A1products.js
@@ -0,0 +1,44 @@
+let products = []
+
+
+const product1 = {
+    brand: "Skittles",
+    price: 2.00,
+    image: "skittles.jpg",
+    description: "3.5oz bag",
+    quantity_available: 20
+},
+
+product2 = {
+    brand: "M&Ms",
+    price: 2.50,
+    image: "",
+    description: "4oz bag",
+    quantity_available: 15
+},
+
+product3 = {
+    brand: "Sour Patch Kids",
+    price: 3.00, 
+    image: "",
+    description: "3.5oz bag",
+    quantity_available: 10
+},
+
+product4 = {
+    brand: "Starburst",
+    price: 4.25,
+    image: "",
+    description: "5oz bag",
+    quantity_available: 7
+},
+
+product5 = {
+    brand: "Haribo Gummy Bears",
+    price: 5.00,
+    image: "",
+    description: "8oz bag",
+    quantity_available: 5
+};
+
+products = [product1, product2, product3, product4, product5];
---
+++ README.md
@@ -0,0 +1,2 @@
+# Matsudo_Zachary_Assignment1
+ 
